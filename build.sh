2e772d38811bd9445ba542dc26f34710ca75b128
Michael A <mikeautorino@proton.me>
2026-01-12 00:10:31 -0500

    build.sh revert

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python ./portfolio/manage.py collectstatic --no-input
+python /manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python ./portfolio/manage.py migrate
+python /manage.py migrate
